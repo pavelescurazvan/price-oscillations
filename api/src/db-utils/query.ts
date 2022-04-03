409049bab88614e86572d732c0e7e7009474d627
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2022-04-04 01:45:36 +0300

    Removed not needed comments

--- api/src/db-utils/query.ts
+++ api/src/db-utils/query.ts
@@ -9,9 +9,6 @@ import {Pool, QueryResult} from 'pg';
 export async function query<Row = any>(pool: Pool, sql: string, bindings: unknown[] = []): Promise<QueryResult<Row>> {
 	const connection = await pool.connect();
 
-	console.log('sql', sql);
-	console.log('bindings', bindings);
-
 	try {
 		return await connection.query<Row>(sql, bindings);
 	} finally {
