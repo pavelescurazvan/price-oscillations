e2037704d63d76b3c0360f30c14e0145ede61de7
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2022-04-04 01:04:18 +0300

    Update SQL

--- api/db/migration/V0_1__create_entries_table.sql
+++ api/db/migration/V0_1__create_entries_table.sql
@@ -1,6 +1,8 @@
+CREATE SCHEMA IF NOT EXISTS tickers;
+
 CREATE TABLE tickers.entries (
   id BIGSERIAL PRIMARY KEY,
   currency_pair TEXT NOT NULL,
   date DATE NOT NULL,
-  price: BIGINT NOT NULL
+  price BIGINT NOT NULL
 );
